c88302bfd694b15e25c4ee16a008fe4c21f04b33
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2020-11-17 01:57:49 +0900

    11.17(2)

--- NUGU/NUGU/settings.py
+++ NUGU/NUGU/settings.py
@@ -114,6 +114,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 SITE_ID= 1
