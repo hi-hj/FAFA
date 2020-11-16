b8a3280e5799bab730158045c5fbb9bb40295c6a
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2020-11-17 01:20:58 +0900

    11.17

--- NUGU/FAFA/views.py
+++ NUGU/FAFA/views.py
@@ -1,6 +1,12 @@
-from rest_framework import viewsets
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+
 from rest_framework.filters import SearchFilter
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+
 
 from .models import Location
 from .serializers import LocationSerializer
