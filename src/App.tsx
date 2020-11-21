487f01c09c1e4a5feda7c47e841d36c5b64ed478
윤승권 <gwon@yunseung-gwon-ui-MacBookPro.local>
2020-11-22 03:33:35 +0900

    current location

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Platform} from 'react-native';
 import {StatusBar} from 'react-native';
 import Navigator from '~/Screens/Navigator';
+import Geolocation from 'react-native-geolocation-service';
+
 interface Props {}
 
 const App = ({ }: Props) => {
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Geolocation.requestAuthorization('always');
+    }
+  }, []);
   return (
     <>
       <StatusBar barStyle="dark-content" />
--- src/Screens/Map/index.tsx
+++ src/Screens/Map/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { NavigationScreenProp, NavigationState } from 'react-navigation';
 import Styled from 'styled-components/native';
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import Geolocation from 'react-native-geolocation-service';
+
 
 const Container = Styled.View`
     flex: 1;
@@ -19,17 +21,56 @@ interface Props {
     navigation: NavigationScreenProp<NavigationState>;
 }
 
+interface ILocation {
+  latitude: number;
+  longitude: number;
+}
+
 const Map = ({ navigation }:Props) => {
+  const [location, setLocation] = useState<ILocation | undefined>(undefined);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
+
   const _logout = () => {
       AsyncStorage.removeItem('key');
       navigation.navigate('LoginNavigator');
   }
-
   return (
     <Container>
-      <MapView style={{flex: 1}} provider={PROVIDER_GOOGLE} 
-      />
-    </Container>
+    {location && (
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <Marker
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+          title="Company"
+        />
+      </MapView>
+    )}
+  </Container>
   );
 };
 
