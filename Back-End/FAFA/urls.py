2355803017481d4c4968e10dfd5152c1a78edd93
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2020-12-02 19:36:42 +0900

    12.02(1)

---
+++ Back-End/FAFA/urls.py
@@ -0,0 +1,43 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+
+from . import views
+
+router = DefaultRouter()
+
+
+router.register('add_location', views.LocationViewSet)
+router.register('set_location', views.SetLocationViewSet)
+router.register('alert', views.AlertViewSet) # Front-End
+router.register('user', views.UserViewSet)
+
+#router.register('answer.location', views.a1_location, basename='a1_location')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('health', views.health, name='health'),
+    path('location', views.location, name="location"),
+    path('now_location', views.location, name="location"),
+    path('between_location', views.location, name="location"),
+    path('except_location', views.location, name="location"),
+
+    path('alert_NUGU', views.alert, name="alert"), # NUGU-PLAY
+    path('test', views.test_location, name="test"),
+    path('login', views.login, name="login"),
+    #path('posts/', views.posts, name='posts'),
+    #path('train', Train.as_view(), name='train'),
+    path('predict', views.predict, name='predict'),
+
+
+
+    #path('login/', views.login),
+    #path('ask.location', views.a1_location, name='ask.location'),
+    # path('ask.location', views.a2_location, name='ask.location'),
+    # path('a3_location', views.a3_location, name='a3_location'),
+]
+#router.register('answer.location', views.C1_LocationView, basename='C1_Location')
+
+
+# router.register('feed_scribe', views.SubscribeVideoViewSet)
+# router.register('upload_video', views.VideoViewSet)
---
+++ Back-End/NUGU/settings.py
@@ -0,0 +1,175 @@
+import os
+from pathlib import Path
+import datetime
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '($efc_3&$$(!8!t2i@dfffs2ophfjope!f)zb!sm3-164dk*v&'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    '*',
+    'NUGU-play-FAFA.eba-tsuiq7em.us-west-2.elasticbeanstalk.com',
+    '127.0.0.1',
+    'localhost',]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    # 아래부터 추가된 부분
+    'rest_framework',
+    'FAFA',
+    'estimators',
+
+    # # allauth
+    # 'allauth',
+    # 'allauth.account',
+    # 'allauth.socialaccount',
+
+    # # providers
+    # 'allauth.socialaccount.providers.google',
+]
+# SOCIALACCOUNT_PROVIDERS = {
+#     'google': {
+#         'SCOPE': [
+#             'profile',
+#             'email',
+#         ],
+#         'AUTH_PARAMS': {
+#             'access_type': 'online',
+#         }
+#     }
+# }
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     ),
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework.authentication.BasicAuthentication',
+#     ),
+# }
+
+# JWT_AUTH = {
+#     'JWT_SECRET_KEY': SECRET_KEY,
+#     'JWT_ALGORITHM': 'HS256',
+#     'JWT_ALLOW_REFRESH': True,
+#     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+#     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=28),
+# }
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'NUGU.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'NUGU.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #str(BASE_DIR / 'db.sqlite3'),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'ko-kr'
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = 'static'
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+SITE_ID= 1
+
+#LOGIN_REDIRECT_URL = '/'
+
+# AUTHENTICATION_BACKENDS = (
+
+#     # Needed to login by username in Django admin, regardless of `allauth`
+#     'django.contrib.auth.backends.ModelBackend',
+
+#     # `allauth` specific authentication methods, such as login by e-mail
+#     'allauth.account.auth_backends.AuthenticationBackend',
+
+# )
