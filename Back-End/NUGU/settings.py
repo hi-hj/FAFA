57d3aa34631753cdb8c595f0009fb16325938bfa
DESKTOP-MT70UEQ\hyeon <hyeongjin5378@daum.net>
2020-12-07 03:14:31 +0900

    12.07(2)

--- Back-End/NUGU/settings.py
+++ Back-End/NUGU/settings.py
@@ -18,7 +18,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '*',
     'NUGU-play-FAFA.eba-anxvafbr.ap-northeast-2.elasticbeanstalk.com',
-    #'NUGU-play-FAFA.eba-tsuiq7em.us-west-2.elasticbeanstalk.com',
+    'NUGU-play-FAFA.eba-tsuiq7em.us-west-2.elasticbeanstalk.com',
     '127.0.0.1',
     'localhost',]
 
